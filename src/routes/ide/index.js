489de35ffd1d2ac73cc796ec780a365b1cd01a21
SillyFreak <sillyfreak.ck@gmail.com>
2018-12-22 00:05:16 +0100

    add a new page with a new component

---
+++ src/routes/ide/index.js
@@ -0,0 +1,26 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import Ide from '../../components/Ide/Ide';
+import Layout from '../../components/Layout';
+
+async function action() {
+  return {
+    title: 'IDE',
+    chunks: ['ide'],
+    component: (
+      <Layout>
+        <Ide />
+      </Layout>
+    ),
+  };
+}
+
+export default action;
