2a0eeba1899fcd62897df253febf62a64a38a9a0
SillyFreak <sillyfreak.ck@gmail.com>
2019-03-18 17:09:51 +0100

    add LocalStrategy for passport; don't check the password

--- src/server.js
+++ src/server.js
@@ -84,6 +84,20 @@ app.use((err, req, res, next) => {
 
 app.use(passport.initialize());
 
+app.post(
+  '/login',
+  passport.authenticate('local', {
+    failureRedirect: '/login',
+    session: false,
+  }),
+  (req, res) => {
+    const expiresIn = 60 * 60 * 24 * 180; // 180 days
+    const token = jwt.sign(req.user, config.auth.jwt.secret, { expiresIn });
+    res.cookie('id_token', token, { maxAge: 1000 * expiresIn, httpOnly: true });
+    res.redirect('/');
+  },
+);
+
 app.get(
   '/login/facebook',
   passport.authenticate('facebook', {
