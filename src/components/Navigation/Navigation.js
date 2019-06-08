4664787a15c2242247474f29e7263767e4e2820e
SillyFreak <sillyfreak.ck@gmail.com>
2019-06-08 17:34:42 +0200

    add components/misc/ for small utility components; move Link there

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -13,7 +13,7 @@ import React from 'react';
 import cx from 'classnames';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Navigation.scss';
-import Link from '../Link';
+import Link from '../misc/Link';
 
 class Navigation extends React.Component<{||}> {
   render() {
