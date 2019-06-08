99bbf475735106fe1563aa26b6ada7d9801bb35e
SillyFreak <sillyfreak.ck@gmail.com>
2019-06-08 09:18:19 +0200

    extract renderHtml

---
+++ src/server/renderHtml.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/server';
+
+import Html from './Html';
+
+export default function renderHtml(rootComponent, data) {
+  const content = ReactDOM.renderToString(rootComponent);
+  const html = ReactDOM.renderToStaticMarkup(<Html {...data}>{content}</Html>);
+  return `<!doctype html>${html}`;
+}
