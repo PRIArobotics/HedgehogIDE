714c7a8fe613f73b72acece0980712128ac581bf
SillyFreak <sillyfreak.ck@gmail.com>
2019-06-08 14:07:19 +0200

    extract IsomorphicStyleLoader, loadScripts

---
+++ src/server/loaders.js
@@ -0,0 +1,32 @@
+// import assets from './asset-manifest.json'; // eslint-disable-line import/no-unresolved
+import chunks from './chunk-manifest.json'; // eslint-disable-line import/no-unresolved
+
+export class IsomorphicStyleLoader {
+  css = new Set();
+
+  // Enables critical path CSS rendering
+  // https://github.com/kriasoft/isomorphic-style-loader
+  insertCss(...styles) {
+    // eslint-disable-next-line no-underscore-dangle
+    styles.forEach(style => this.css.add(style._getCss()));
+  }
+
+  collect() {
+    return [...this.css].join('');
+  }
+}
+
+export function loadScripts(...chnks) {
+  const scripts = new Set();
+
+  chnks.forEach(chunk => {
+    if (chunks[chunk]) {
+      // add all assets belonging to that chunk
+      chunks[chunk].forEach(asset => scripts.add(asset));
+    } else if (__DEV__) {
+      throw new Error(`Chunk with name '${chunk}' cannot be found`);
+    }
+  });
+
+  return [...scripts];
+}
