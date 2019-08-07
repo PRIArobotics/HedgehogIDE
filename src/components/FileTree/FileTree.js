6873477d740163a6e0801191b81cb95258e76386
Yuhang Jiang <yjiang@student.tgm.ac.at>
2019-08-07 09:46:09 +0200

    FileTree Component

---
+++ src/components/FileTree/FileTree.js
@@ -0,0 +1,127 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import Tree from 'rc-tree';
+import s from 'rc-tree/assets/index.css';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+
+const treeData = [
+  {
+    key: '0-0',
+    title: 'Root',
+    children: [
+      {
+        key: '0-0-0',
+        title: 'Folder #1',
+        children: [{ key: '0-0-0-0', title: 'File #1' }],
+      },
+      {
+        key: '0-0-1',
+        title: 'Folder #2',
+        children: [
+          { key: '0-0-1-0', title: 'File #2' },
+          { key: '0-0-1-1', title: 'File #3' },
+        ],
+      },
+    ],
+  },
+];
+
+class FileTree extends React.Component {
+  static propTypes = {
+    keys: PropTypes.array,
+  };
+
+  static defaultProps = {
+    keys: ['0-0-0-0'],
+  };
+
+  constructor(props) {
+    super(props);
+    const { keys } = props;
+    this.state = {
+      defaultExpandedKeys: keys,
+      defaultSelectedKeys: keys,
+      defaultCheckedKeys: keys,
+    };
+  }
+
+  onExpand = (...args) => {
+    console.log('onExpand', ...args);
+  };
+
+  onSelect = (selectedKeys, info) => {
+    console.log('selected', selectedKeys, info);
+    this.selKey = info.node.props.eventKey;
+
+    if (this.tree) {
+      console.log(
+        'Selected DOM node:',
+        selectedKeys.map(key =>
+          ReactDOM.findDOMNode(this.tree.domTreeNodes[key]),
+        ),
+      );
+    }
+  };
+
+  onCheck = (checkedKeys, info) => {
+    console.log('onCheck', checkedKeys, info);
+  };
+
+  onEdit = () => {
+    setTimeout(() => {
+      console.log('current key: ', this.selKey);
+    }, 0);
+  };
+
+  onDel = e => {
+    if (!window.confirm('sure to delete?')) {
+      return;
+    }
+    e.stopPropagation();
+  };
+
+  setTreeRef = tree => {
+    this.tree = tree;
+  };
+
+  render() {
+    /*
+    const customLabel = (
+      <span className="cus-label">
+        <span>operations: </span>
+        <span style={{ color: 'blue' }} onClick={this.onEdit}>
+          Edit
+        </span>
+        &nbsp;
+        <label onClick={e => e.stopPropagation()}>
+          <input type="checkbox" /> checked
+        </label>
+        &nbsp;
+        <span style={{ color: '#EB0000' }} onClick={this.onDel}>
+          Delete
+        </span>
+      </span>
+    );
+    */
+    return (
+      <div style={{ margin: '0 20px' }}>
+        <Tree
+          className="myCls"
+          showLine
+          checkable={false}
+          selectable={false}
+          defaultExpandAll
+          onExpand={this.onExpand}
+          defaultSelectedKeys={this.state.defaultSelectedKeys}
+          defaultCheckedKeys={this.state.defaultCheckedKeys}
+          onSelect={this.onSelect}
+          onCheck={this.onCheck}
+          treeData={treeData}
+        />
+      </div>
+    );
+  }
+}
+
+export default withStyles(s)(FileTree);
