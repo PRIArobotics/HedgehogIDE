58d1c267ae434c077c3c70ef789ab8830d9b2ad2
Yuchen Jiang <yjiang01@student.tgm.ac.at>
2019-08-09 08:29:06 +0200

    input text will show on screen, but won't recognize html

--- src/components/Console/Console.js
+++ src/components/Console/Console.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './console.css';
@@ -14,16 +15,17 @@ class Console extends React.Component {
   onSubmit = e => {
     e.preventDefault();
     console.log(this.inputRef.current.value);
-    consoleText += `${this.inputRef.current.value}<br />`;
+    consoleText += this.inputRef.current.value;
+    consoleText += '<br />';
+    ReactDOM.render(consoleText, document.getElementById('text1'));
     this.inputRef.current.value = '';
-    this.setState(this.state);
   };
 
   render() {
     return (
       <div className={s.console}>
         Hallo! <br />
-        {consoleText}
+        <div id="text1" />
         <form onSubmit={this.onSubmit}>
           User&gt; <input type="text" name="name" className={s.console} ref={this.inputRef} />
         </form>
