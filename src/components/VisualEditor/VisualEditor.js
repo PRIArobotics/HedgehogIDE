03ee88647a23c4188e01ef4ba86c195ac5aabaf7
Yuchen Jiang <yjiang01@student.tgm.ac.at>
2019-08-19 13:19:12 +0200

    Visual Editor Toolbar extended

--- src/components/VisualEditor/VisualEditor.js
+++ src/components/VisualEditor/VisualEditor.js
@@ -11,20 +11,22 @@ class VisualEditor extends React.Component {
       <React.Fragment>
         <div id="blocklyDiv" style={{ height: '600px', width: '1000px' }} />
         <xml id="toolbox" style={{ display: 'none' }} ref={(toolbox) => {this.toolbox = toolbox}}>
-          <category name="Control" colour="290">
+          <category name="Logic" colour="210">
             <block type="controls_if" />
-            <block type="controls_whileUntil" />
-            <block type="controls_for" />
-          </category>
-          <category name="Logic" colour="120">
             <block type="logic_compare" />
             <block type="logic_operation" />
             <block type="logic_boolean" />
+          </category>
+          <category name="Loops" colour="120">
+            <block type="controls_whileUntil" />
+            <block type="controls_for" />
           </category>
           <category name="Math" colour="230">
             <block type="math_number" />
             <block type="math_arithmetic" />
           </category>
+          <category name="Variables" custom="VARIABLE" colour="330" />
+          <category name="Functions" custom="PROCEDURE" colour="290" />
         </xml>
       </React.Fragment>
     );
