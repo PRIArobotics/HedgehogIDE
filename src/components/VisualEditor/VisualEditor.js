9db2189c2a664c2ba4b97f5fc6eb2e688e313402
Yuchen Jiang <yjiang01@student.tgm.ac.at>
2019-08-19 13:03:58 +0200

    Visual Editor now has a extensive Toolbar

--- src/components/VisualEditor/VisualEditor.js
+++ src/components/VisualEditor/VisualEditor.js
@@ -10,14 +10,21 @@ class VisualEditor extends React.Component {
     return (
       <React.Fragment>
         <div id="blocklyDiv" style={{ height: '600px', width: '1000px' }} />
-        <xml id="toolbox" style={{ display: 'none' }} ref={(toolbox) => this.toolbox = toolbox} >
-          <block type="controls_if" />
-          <block type="controls_repeat_ext" />
-          <block type="logic_compare" />
-          <block type="math_number" />
-          <block type="math_arithmetic" />
-          <block type="text" />
-          <block type="text_print" />
+        <xml id="toolbox" style={{ display: 'none' }} ref={(toolbox) => {this.toolbox = toolbox}}>
+          <category name="Control" colour="290">
+            <block type="controls_if" />
+            <block type="controls_whileUntil" />
+            <block type="controls_for" />
+          </category>
+          <category name="Logic" colour="120">
+            <block type="logic_compare" />
+            <block type="logic_operation" />
+            <block type="logic_boolean" />
+          </category>
+          <category name="Math" colour="230">
+            <block type="math_number" />
+            <block type="math_arithmetic" />
+          </category>
         </xml>
       </React.Fragment>
     );
