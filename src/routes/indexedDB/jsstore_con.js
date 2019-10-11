5ada757283a66aab7645cffc1d03ad096846940c
Clemens Koza <koza@pria.at>
2019-10-11 12:17:23 +0200

    require not in a function; gets rid of a lint

--- src/routes/indexedDB/jsstore_con.js
+++ src/routes/indexedDB/jsstore_con.js
@@ -1,14 +1,9 @@
 import * as JsStore from 'jsstore';
 
-const getWorkerPath = () => {
-  // return dev build when env is development
-  if (process.env.NODE_ENV === 'development') {
-    return require('jsstore/dist/jsstore.worker');
-  }
-  // return prod build when env is production
-  return require('jsstore/dist/jsstore.worker.min');
-};
+const workerPath =
+  process.env.NODE_ENV === 'production'
+    ? require('jsstore/dist/jsstore.worker.min')
+    : require('jsstore/dist/jsstore.worker');
 
-const workerPath = getWorkerPath();
 const connection = new JsStore.Instance(new Worker(workerPath));
 export default connection;
