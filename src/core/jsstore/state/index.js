3afb8443d99b1d7b3074d8bcc763a6cf7420522e
Clemens Koza <koza@pria.at>
2019-10-15 13:49:00 +0200

    don't use a StateDB class, just export some DAO functions

--- src/core/jsstore/state/index.js
+++ src/core/jsstore/state/index.js
@@ -25,44 +25,32 @@ const schema = {
 
 const connection = connect(schema);
 
-class StateDB {
-  connection: Promise<JsStore.Instance>;
-
-  // eslint-disable-next-line no-shadow
-  constructor(connection: Promise<JsStore.Instance>) {
-    this.connection = connection;
-  }
-
-  // if you want to check initialization before making the first store access
-  // it doesn't do anything, but if initialization failed,
-  // this will be a rejected promise.
-  async init() {
-    await this.connection;
-  }
-
-  async getState(id) {
-    const connection = await this.connection;
-
-    const result = await connection.select({
-      from: 'States',
-      where: [{ id }],
-    });
-
-    // eslint-disable-next-line no-throw-literal
-    if (result.length === 0) throw 'not found';
-
-    return result[0].state;
-  }
-
-  async setState(id, state) {
-    const connection = await this.connection;
-
-    await connection.insert({
-      into: 'States',
-      values: [{ id, state }],
-      upsert: true,
-    });
-  }
+// if you want to check initialization before making the first store access
+// it doesn't do anything, but if initialization failed, this will be a rejected promise.
+export async function init() {
+  await connection;
 }
 
-export default new StateDB(connection);
+export async function getState(id) {
+  const conn = await connection;
+
+  const result = await conn.select({
+    from: 'States',
+    where: [{ id }],
+  });
+
+  // eslint-disable-next-line no-throw-literal
+  if (result.length === 0) throw 'not found';
+
+  return result[0].state;
+}
+
+export async function setState(id, state) {
+  const conn = await connection;
+
+  await conn.insert({
+    into: 'States',
+    values: [{ id, state }],
+    upsert: true,
+  });
+}
