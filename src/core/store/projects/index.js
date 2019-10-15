4e4420523b76d35f6f6a6017abba27422aebb40b
Clemens Koza <koza@pria.at>
2019-10-15 15:36:25 +0200

    add Projects CRUD code and simple test to verify the logic

---
+++ src/core/store/projects/index.js
@@ -0,0 +1,109 @@
+// @flow
+
+import * as JsStore from 'jsstore';
+
+import connect from '../connect';
+
+export type Project = {|
+  id?: number,
+  name: string,
+|};
+
+const tblProjects = {
+  name: 'Projects',
+  columns: {
+    id: {
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      dataType: 'string',
+      notNull: true,
+      unique: true,
+    },
+  },
+};
+
+const connection = connect({
+  name: 'Projects',
+  tables: [tblProjects],
+});
+
+function unique<T>(result: T[]): T {
+  // eslint-disable-next-line no-throw-literal
+  if (result.length === 0) throw 'not found';
+  // eslint-disable-next-line no-throw-literal
+  if (result.length > 1) throw 'not unique';
+  return result[0];
+}
+
+// if you want to check initialization before making the first store access
+// it doesn't do anything, but if initialization failed, this will be a rejected promise.
+export async function init() {
+  await connection;
+}
+
+export async function getProjects(): Promise<Project[]> {
+  const conn = await connection;
+
+  return await conn.select({
+    from: 'Projects',
+  });
+}
+
+export async function getProjectById(id: number): Promise<Project> {
+  const conn = await connection;
+
+  return unique(
+    await conn.select({
+      from: 'Projects',
+      where: { id },
+    })
+  );
+}
+
+export async function getProjectByName(name: string): Promise<Project[]> {
+  const conn = await connection;
+
+  return unique(
+    await conn.select({
+      from: 'Projects',
+      where: { name },
+    })
+  );
+}
+
+export async function createProject(project: Project): Promise<void> {
+  const conn = await connection;
+
+  const [{ id }] = await conn.insert({
+    into: 'Projects',
+    values: [project],
+    return: true,
+  });
+
+  project.id = id;
+}
+
+export async function updateProject(project: Project): Promise<void> {
+  const conn = await connection;
+
+  const { id, ...values } = project;
+  await conn.update({
+    in: 'Projects',
+    set: values,
+    where: { id },
+  });
+}
+
+export async function removeProject(project: Project): Promise<void> {
+  const conn = await connection;
+
+  const { id } = project;
+  await conn.remove({
+    from: 'Projects',
+    where: { id },
+  });
+
+  project.id = id;
+}
--- src/routes/indexedDB/IndexedDB.js
+++ src/routes/indexedDB/IndexedDB.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import * as StateDB from '../../core/store/state';
+import * as ProjectsDB from '../../core/store/projects';
 
 export default class IndexedDB extends React.Component {
   inputRef: React.RefObject = React.createRef();
@@ -11,6 +12,32 @@ export default class IndexedDB extends React.Component {
     (async () => {
       await StateDB.init();
       this.inputRef.current.value = await StateDB.getState('Input');
+
+      await ProjectsDB.init();
+
+      console.log('empty', await ProjectsDB.getProjects());
+
+      let p: ProjectsDB.Project = { name: 'foo' };
+      console.log('new object', p);
+
+      await ProjectsDB.createProject(p);
+      console.log('created', p);
+
+      console.log('created', await ProjectsDB.getProjects());
+
+      try {
+        await ProjectsDB.createProject(p);
+      } catch(ex) {}
+
+      p.name = 'bar';
+      await ProjectsDB.updateProject(p);
+      console.log('updated', await ProjectsDB.getProjects());
+
+      console.log(await ProjectsDB.getProjectById(p.id));
+      console.log(await ProjectsDB.getProjectByName('bar'));
+
+      await ProjectsDB.removeProject(p);
+      console.log('removed', await ProjectsDB.getProjects());
     })();
   }
 
