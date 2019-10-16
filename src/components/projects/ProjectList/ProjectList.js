742a7a36410581963af893c6fd87e1c614e2b4de
Clemens Koza <koza@pria.at>
2019-10-16 19:17:25 +0200

    add project deletion, fix lints

--- src/components/projects/ProjectList/ProjectList.js
+++ src/components/projects/ProjectList/ProjectList.js
@@ -1,6 +1,7 @@
 // @flow
 
 import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -12,10 +13,15 @@ import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import FolderIcon from '@material-ui/icons/Folder';
 import DeleteIcon from '@material-ui/icons/Delete';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 import Link from '../../misc/Link';
 
-import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './ProjectList.scss';
 
 import * as ProjectsDB from '../../../core/store/projects';
@@ -23,18 +29,38 @@ import * as ProjectsDB from '../../../core/store/projects';
 type PropTypes = {||};
 type StateTypes = {|
   projects: Array<ProjectsDB.Project>,
+  projectToDelete: ProjectsDB.Project | null,
 |};
 
 class Console extends React.Component<PropTypes, StateTypes> {
   state: StateTypes = {
     projects: [],
+    projectToDelete: null,
   };
 
   componentDidMount() {
     (async () => {
-      const projects = await ProjectsDB.getProjects();
-      this.setState({ projects });
+      await this.refreshProjects();
     })();
+  }
+
+  async refreshProjects() {
+    const projects = await ProjectsDB.getProjects();
+    this.setState({ projects });
+  }
+
+  beginDeleteProject(project: ProjectsDB.Project) {
+    this.setState({ projectToDelete: project });
+  }
+
+  cancelDeleteProject() {
+    this.setState({ projectToDelete: null });
+  }
+
+  async confirmDeleteProject() {
+    await ProjectsDB.removeProject(this.state.projectToDelete);
+    await this.refreshProjects();
+    this.setState({ projectToDelete: null });
   }
 
   render() {
@@ -42,29 +68,64 @@ class Console extends React.Component<PropTypes, StateTypes> {
       <div className={s.root}>
         <div className={s.container}>
           <h1>Your projects</h1>
-          <List>{this.state.projects.map(project => (
-            <ListItem
-              key={project.id}
-              button
-              component={Link}
-              to={`/projects/${project.name}`}
-            >
-              <ListItemAvatar>
-                <Avatar>
-                  <FolderIcon />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary={project.name}
-                // secondary="Secondary text"
-              />
-              <ListItemSecondaryAction>
-                <IconButton edge="end" aria-label="delete">
-                  <DeleteIcon />
-                </IconButton>
-              </ListItemSecondaryAction>
-            </ListItem>
-          ))}</List>
+          <List>
+            {this.state.projects.map(project => (
+              <ListItem
+                key={project.id}
+                button
+                component={Link}
+                to={`/projects/${project.name}`}
+              >
+                <ListItemAvatar>
+                  <Avatar>
+                    <FolderIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary={project.name}
+                  // secondary="Secondary text"
+                />
+                <ListItemSecondaryAction>
+                  <IconButton
+                    edge="end"
+                    aria-label="delete"
+                    onClick={() => this.beginDeleteProject(project)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            ))}
+          </List>
+          <Dialog
+            open={this.state.projectToDelete !== null}
+            onClose={() => this.cancelDeleteProject()}
+            aria-labelledby="delete-dialog-title"
+            aria-describedby="delete-dialog-description"
+          >
+            <DialogTitle id="delete-dialog-title">Confirm deletion</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="delete-dialog-description">
+                Are you sure yo want to delete project &quot;
+                {(this.state.projectToDelete || {}).name}&quot;?
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button
+                onClick={() => this.cancelDeleteProject()}
+                color="secondary"
+              >
+                Cancel
+              </Button>
+              <Button
+                onClick={() => this.confirmDeleteProject()}
+                color="primary"
+                autoFocus
+              >
+                OK
+              </Button>
+            </DialogActions>
+          </Dialog>
         </div>
       </div>
     );
