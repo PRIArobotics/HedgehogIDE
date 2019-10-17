25247d13b0254d4a79bb61b7b5cb080d9f8e1745
Clemens Koza <koza@pria.at>
2019-10-17 15:35:04 +0200

    improve errors in projects store and its usage in ProjectList component

--- src/core/store/projects/index.js
+++ src/core/store/projects/index.js
@@ -27,11 +27,15 @@ const connection = connect({
   tables: [tblProjects],
 });
 
+export class ProjectError extends Error {
+  name = 'ProjectError';
+}
+
 function unique<T>(result: T[]): T {
   // eslint-disable-next-line no-throw-literal
-  if (result.length === 0) throw 'not found';
+  if (result.length === 0) throw new ProjectError('no project found');
   // eslint-disable-next-line no-throw-literal
-  if (result.length > 1) throw 'not unique';
+  if (result.length > 1) throw new ProjectError('multiple projects found');
   return result[0];
 }
 
@@ -74,33 +78,44 @@ export async function getProjectByName(name: string): Promise<Project[]> {
 export async function createProject(project: Project): Promise<void> {
   const conn = await connection;
 
-  const [{ id }] = await conn.insert({
-    into: 'Projects',
-    values: [project],
-    return: true,
-  });
+  try {
+    const [{ id }] = await conn.insert({
+      into: 'Projects',
+      values: [project],
+      return: true,
+    });
 
-  // eslint-disable-next-line no-param-reassign
-  project.id = id;
+    // eslint-disable-next-line no-param-reassign
+    project.id = id;
+  } catch (ex) {
+    // whatever happened here
+    if (typeof ex !== 'object' || ex.type !== 'ConstraintError') throw ex;
+
+    throw new ProjectError('constraint was violated at insertion');
+  }
 }
 
 export async function updateProject(project: Project): Promise<void> {
   const conn = await connection;
 
   const { id, ...values } = project;
-  await conn.update({
+  const rows = await conn.update({
     in: 'Projects',
     set: values,
     where: { id },
   });
+
+  if (rows !== 1) throw new ProjectError('project not found');
 }
 
 export async function removeProject(project: Project): Promise<void> {
   const conn = await connection;
 
   const { id } = project;
-  await conn.remove({
+  const rows = await conn.remove({
     from: 'Projects',
     where: { id },
   });
+
+  if (rows !== 1) throw new ProjectError('project not found');
 }
