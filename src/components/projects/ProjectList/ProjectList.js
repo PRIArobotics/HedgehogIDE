0e1c7ca467785f9c88e87426109f6e66746846f8
Clemens Koza <koza@pria.at>
2019-10-29 10:28:15 +0100

    remove obsolete imports

--- src/components/projects/ProjectList/ProjectList.js
+++ src/components/projects/ProjectList/ProjectList.js
@@ -18,13 +18,6 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import FolderIcon from '@material-ui/icons/Folder';
 import RefreshIcon from '@material-ui/icons/Refresh';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 
 import Link from '../../misc/Link';
 import * as ProjectsDB from '../../../core/store/projects';
