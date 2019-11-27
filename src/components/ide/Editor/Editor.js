8c28ad6d43d95c0a5d161c95d54fd0bbb86087ee
Clemens Koza <koza@pria.at>
2019-11-27 12:58:55 +0100

    persist editor contents to real files. this by accident removes the need for the `initial` variable that prevents the first render: loading the file asynchronously does the same thing

--- src/components/ide/Editor/Editor.js
+++ src/components/ide/Editor/Editor.js
@@ -12,24 +12,24 @@ import { styled } from '@material-ui/styles';
 import IconButton from '@material-ui/core/IconButton';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 
+import filer, { fs } from 'filer';
+
+import { Project } from '../../../core/store/projects';
+
 import s from './Editor.scss';
 
-export type ControlledState = $Shape<{|
-  text: string,
-|}>;
-
 type PropTypes = {|
-  text: string,
-  onUpdate: (state: ControlledState) => void | Promise<void>,
+  project: Project,
+  path: string,
   onExecute: (code: string) => void | Promise<void>,
   onTerminate: () => void | Promise<void>,
   running: boolean,
   layoutNode: any,
 |};
 type StateTypes = {|
-  initial: boolean,
-  editorHeight: string,
+  content: string | null,
   editorWidth: string,
+  editorHeight: string,
 |};
 
 const ColoredIconButton = styled(({ color, ...other }) => (
@@ -44,13 +44,49 @@ class Editor extends React.Component<PropTypes, StateTypes> {
   editorRef: RefObject<typeof AceEditor> = React.createRef();
 
   state = {
-    initial: true,
+    content: null,
     editorWidth: '0',
     editorHeight: '0',
   };
 
+  constructor(props: PropTypes) {
+    super(props);
+
+    this.refreshFile();
+  }
+
+  async refreshFile() {
+    const { project, path } = this.props;
+    const absolutePath = project.resolve(path);
+
+    const content = await fs.promises.readFile(absolutePath, 'utf8');
+    this.setState({ content });
+  }
+
+  setContent(content: string) {
+    // only set content if the current content is not null,
+    // i.e. the original file contents were loaded successfully,
+    // and subsequently persist that content
+    this.setState(
+      ({ content: oldContent }) => (oldContent === null ? {} : { content }),
+      () => this.save(),
+    );
+  }
+
+  async save() {
+    const { project, path } = this.props;
+    const { content } = this.state;
+
+    // if the content was not loaded yet for whatever reason,
+    // don't delete existing file contents.
+    if (content === null) return;
+
+    const absolutePath = project.resolve(path);
+
+    await fs.promises.writeFile(absolutePath, this.state.content, 'utf8');
+  }
+
   componentDidMount() {
-    this.setState({ initial: false });
     this.props.layoutNode.setEventListener('resize', this.handleResize);
     this.props.layoutNode.setEventListener('visibility', this.handleResize);
     this.handleResize();
@@ -60,22 +96,21 @@ class Editor extends React.Component<PropTypes, StateTypes> {
     setTimeout(() => {
       if (this.containerRef.current !== null) {
         this.setState({
-          editorHeight: `${this.containerRef.current.offsetHeight}px`,
           editorWidth: `${this.containerRef.current.offsetWidth}px`,
+          editorHeight: `${this.containerRef.current.offsetHeight}px`,
         });
       }
     }, 0);
   };
 
   render() {
-    // not rendering directly after the component was mounted
-    // somehow fixes a strange display bug.
-    if (this.state.initial) return null;
+    const { running } = this.props;
+    const { content, editorWidth, editorHeight } = this.state;
 
     return (
       <div className={s.root}>
         <div className={s.sidebar}>
-          {this.props.running ? (
+          {running ? (
             <ColoredIconButton
               onClick={() => this.props.onTerminate()}
               disableRipple
@@ -85,7 +120,7 @@ class Editor extends React.Component<PropTypes, StateTypes> {
             </ColoredIconButton>
           ) : (
             <ColoredIconButton
-              onClick={() => this.props.onExecute(this.props.text)}
+              onClick={() => this.props.onExecute(content)}
               disableRipple
               color="limegreen"
             >
@@ -95,33 +130,35 @@ class Editor extends React.Component<PropTypes, StateTypes> {
           <br />
         </div>
         <div className={s.container} ref={this.containerRef}>
-          <AceEditor
-            mode="javascript"
-            name="editor"
-            height={this.state.editorHeight}
-            width={this.state.editorWidth}
-            ref={this.editorRef}
-            // onLoad={this.onLoad}
-            onChange={text => this.props.onUpdate({ text })}
-            fontSize={16}
-            // onSelectionChange={this.onSelectionChange}
-            // onCursorChange={this.onCursorChange}
-            // onValidate={this.onValidate}
-            value={this.props.text}
-            showGutter
-            highlightActiveLine
-            autoScrollEditorIntoView
-            style={{
-              position: 'absolute',
-            }}
-            setOptions={{
-              enableBasicAutocompletion: true,
-              enableLiveAutocompletion: true,
-              // enableSnippets: this.state.enableSnippets,
-              showLineNumbers: true,
-              tabSize: 2,
-            }}
-          />
+          {content === null ? null : (
+            <AceEditor
+              mode="javascript"
+              name="editor"
+              width={editorWidth}
+              height={editorHeight}
+              ref={this.editorRef}
+              // onLoad={this.onLoad}
+              onChange={text => this.setContent(text)}
+              fontSize={16}
+              // onSelectionChange={this.onSelectionChange}
+              // onCursorChange={this.onCursorChange}
+              // onValidate={this.onValidate}
+              value={content}
+              showGutter
+              highlightActiveLine
+              autoScrollEditorIntoView
+              style={{
+                position: 'absolute',
+              }}
+              setOptions={{
+                enableBasicAutocompletion: true,
+                enableLiveAutocompletion: true,
+                // enableSnippets: this.state.enableSnippets,
+                showLineNumbers: true,
+                tabSize: 2,
+              }}
+            />
+          )}
         </div>
       </div>
     );
