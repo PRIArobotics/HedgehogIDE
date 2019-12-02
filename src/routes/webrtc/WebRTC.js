54c96db40823b7e1249c65bfc48634d9aca82390
Clemens Koza <koza@pria.at>
2019-12-02 15:26:25 +0100

    implement with WebRTC

--- src/routes/webrtc/WebRTC.js
+++ src/routes/webrtc/WebRTC.js
@@ -12,37 +12,109 @@ type Message = {|
   text: string,
 |};
 
+type Chat = {|
+  messages: Array<Message>,
+  connection: RTCPeerConnection,
+  channel: RTCDataChannel,
+|};
+
 const msg = (type: 'IN' | 'OUT', text: string): Message => ({ type, text });
 
 type PropTypes = {||};
 type StateTypes = {|
-  left: Array<Message>,
-  right: Array<Message>,
+  left: Chat | null,
+  right: Chat | null,
 |};
 
 class WebRTC extends React.Component<PropTypes, StateTypes> {
   state = {
-    left: [],
-    right: [],
+    left: null,
+    right: null,
+  };
+
+  async createConnections() {
+    const leftConnection = new RTCPeerConnection(null);
+    const rightConnection = new RTCPeerConnection(null);
+    leftConnection.onicecandidate = async ({ candidate }) => {
+      await rightConnection.addIceCandidate(candidate);
+      console.log("left ICE candidate added to right");
+    };
+    rightConnection.onicecandidate = async ({ candidate }) => {
+      await leftConnection.addIceCandidate(candidate);
+      console.log("right ICE candidate added to left");
+    };
+
+    console.log('left & right created');
+
+    const leftChannel = leftConnection.createDataChannel('chat');
+    console.log('channel created @ left');
+
+    const offer = await leftConnection.createOffer();
+    leftConnection.setLocalDescription(offer);
+    rightConnection.setRemoteDescription(offer);
+    console.log('left offered right');
+    const answer = await rightConnection.createAnswer();
+    console.log('right answered left');
+    rightConnection.setLocalDescription(answer);
+    leftConnection.setRemoteDescription(answer);
+
+    const rightChannel = await new Promise((resolve, reject) => {
+      rightConnection.ondatachannel = ({ channel }) => resolve(channel);
+    });
+    console.log('channel connected @ right');
+
+    leftChannel.onmessage = ({ data }) => this.handleRecv('left', data);
+    rightChannel.onmessage = ({ data }) => this.handleRecv('right', data);
+
+    this.setState({
+      left: {
+        messages: [],
+        connection: leftConnection,
+        channel: leftChannel,
+      },
+      right: {
+        messages: [],
+        connection: rightConnection,
+        channel: rightChannel,
+      },
+    })
   };
 
   componentDidMount() {
+    this.createConnections();
   }
 
-  handleSend(from: string, to: string) {
-    const text = 'msg';
+  handleRecv(to: string, text: string) {
+    this.setState(oldState => {
+      const chat = oldState[to];
+      return {
+        [to]: {
+          ...chat,
+          messages: [...chat.messages, msg('IN', text)],
+        },
+      };
+    });
+  };
+
+  handleSend(from: string, text: string) {
     return e => {
-      this.setState(oldState => ({
-        [from]: [...oldState[from], msg('OUT', text)],
-        [to]: [...oldState[to], msg('IN', text)],
-      }));
+      this.state[from].channel.send(text);
+      this.setState(oldState => {
+        const chat = oldState[from];
+        return {
+          [from]: {
+            ...chat,
+            messages: [...chat.messages, msg('OUT', text)],
+          },
+        };
+      });
     };
   }
 
   render() {
-    const renderChat = (messages: Array<Message>, onSend) => (
+    const renderChat = (chat: Chat, onSend) => (
       <Paper className={s.chat} square>
-        {messages.map(({ type, text }, i) => (
+        {chat.messages.map(({ type, text }, i) => (
           <div key={i} className={`${s.msg} ${type === 'OUT' ? s.mine : s.theirs}`}>{text}</div>
         ))}
         <button onClick={onSend}>Send</button>
@@ -52,8 +124,8 @@ class WebRTC extends React.Component<PropTypes, StateTypes> {
     return (
       <div className={s.root}>
         <div className={s.container}>
-          {renderChat(this.state.left, this.handleSend('left', 'right'))}
-          {renderChat(this.state.right, this.handleSend('right', 'left'))}
+          {this.state.left === null ? null : renderChat(this.state.left, this.handleSend('left', 'Hello'))}
+          {this.state.right === null ? null : renderChat(this.state.right, this.handleSend('right', 'There'))}
         </div>
       </div>
     );
