a6e22baa0b2a396bee9ca0a97b743e1c56431652
Clemens Koza <koza@pria.at>
2019-12-06 11:12:01 +0100

    basic Workbox service worker setup; does not yet cache the basic routes which means it still fails when the server is offline

---
+++ src/serviceworker/index.js
@@ -0,0 +1,10 @@
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
