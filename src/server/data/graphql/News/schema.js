9993c0f3b04b0c30b25b4586d60bb7a2cd2b1b7e
Clemens Koza <koza@pria.at>
2019-12-10 17:34:23 +0100

    get rid of the old landing page

--- src/server/data/graphql/News/schema.js
+++
@@ -1,7 +0,0 @@
-// @flow
-
-import { merge } from '../../../../core/graphql/graphqlDef';
-
-import GetAllReactJSNews from './reactjsnews.com/GetAllReactJSNews';
-
-export default merge(GetAllReactJSNews);
--- src/server/data/graphql/schema.js
+++ src/server/data/graphql/schema.js
@@ -3,8 +3,7 @@
 import { merge } from '../../../core/graphql/graphqlDef';
 
 import Apollo from './Apollo/schema';
-import News from './News/schema';
 import Database from './Database/schema';
 import Scalar from './Scalar/schema';
 
-export default merge(Apollo, News, Database, Scalar);
+export default merge(Apollo, Database, Scalar);
