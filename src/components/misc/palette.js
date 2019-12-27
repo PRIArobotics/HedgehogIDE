81648aa8c4915cf3174851cca23def1cb86a5630
Clemens Koza <koza@pria.at>
2019-12-27 14:40:35 +0100

    add sidebar icons to palette

--- src/components/misc/palette.js
+++ src/components/misc/palette.js
@@ -9,6 +9,12 @@ import FolderOpenIcon from 'mdi-material-ui/FolderOpen';
 import LanguageBlocklyIcon from 'mdi-material-ui/Widgets';
 import LanguageJavascriptIcon from 'mdi-material-ui/LanguageJavascript';
 
+// IDE navigation
+import IdeIcon from 'mdi-material-ui/CodeTags';
+import ControlsIcon from 'mdi-material-ui/TuneVertical';
+import ApolloTestIcon from 'mdi-material-ui/Graphql';
+import IndexDBTestIcon from 'mdi-material-ui/Database';
+
 // IDE tab components
 import ConsoleIcon from 'mdi-material-ui/Console';
 import SimulatorIcon from 'mdi-material-ui/AxisArrow';
@@ -20,6 +26,7 @@ import DeleteIcon from 'mdi-material-ui/Delete';
 export {
   FileIcon, FolderIcon, FolderOpenIcon,
   LanguageBlocklyIcon, LanguageJavascriptIcon,
+  IdeIcon, ControlsIcon, ApolloTestIcon, IndexDBTestIcon,
   ConsoleIcon, SimulatorIcon,
   RenameIcon, DeleteIcon,
 };
