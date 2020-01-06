9f2bb454a7beeac1e7bb745568270d0f98a9534e
Clemens Koza <koza@pria.at>
2020-01-06 14:17:20 +0100

    add message code generation, use messages properly using %{}

--- gsl_blockly/__init__.py
+++ gsl_blockly/__init__.py
@@ -10,18 +10,26 @@ def get_model(model_file, msg_model_file):
     def augment_module(name, mod):
         def augment_block(name, block):
             block.name = name
-            block.langs = {}
             return block
+
+        def augment_lang(key, lang):
+            def augment_block(name, block):
+                block.name = name
+                return block
+
+            blocks = [augment_block(*pair) for pair in lang.items()]
+            lang.clear()
+            lang.key = key
+            lang.blocks = blocks
+
+            return lang
 
         blocks = [augment_block(*pair) for pair in mod.items()]
         mod.clear()
         mod.name = name
         mod.blocks = blocks
         if name in msg_model.modules:
-            for lang, lang_blocks in msg_model.modules[name].items():
-                for block in mod.blocks:
-                    if block.name in lang_blocks:
-                        block.langs[lang] = lang_blocks[block.name]
+            mod.langs = [augment_lang(*pair) for pair in msg_model.modules[name].items()]
 
         return mod
 
---
+++ src/components/ide/VisualEditor/blocks/hedgehog_msg_de.js
@@ -0,0 +1,102 @@
+// @flow
+/* eslint-disable */
+
+import Blockly from 'blockly';
+
+Blockly.Msg['HEDGEHOG_SCOPE'] = 'Hedgehog scope %1 %2';
+Blockly.Msg['HEDGEHOG_SCOPE_TOOLTIP'] = 'Alle Hedgehog Blöcke müssen sich innerhalb eines Hedgehog scope Blocks befinden';
+
+Blockly.Msg['HEDGEHOG_MOVE'] = 'bewege Motor %1 %2 für %3 Sekunden';
+Blockly.Msg['HEDGEHOG_MOVE_TOOLTIP'] = 'Bewegt einen Motor in eine bestimmte Richtung';
+
+Blockly.Msg['HEDGEHOG_MOVE_UNLIMITED'] = 'bewege Motor %1 %2';
+Blockly.Msg['HEDGEHOG_MOVE_UNLIMITED_TOOLTIP'] = 'bewegt einen Motor solange bis er explizit gestoppt wird';
+
+Blockly.Msg['HEDGEHOG_MOTOR_OFF'] = 'schalte Motor %1 aus';
+Blockly.Msg['HEDGEHOG_MOTOR_OFF_TOOLTIP'] = 'schaltet einen Motor aus';
+
+Blockly.Msg['HEDGEHOG_BRAKE'] = 'bremse Motor %1';
+Blockly.Msg['HEDGEHOG_BRAKE_TOOLTIP'] = 'bremst einen Motor sofort';
+
+Blockly.Msg['HEDGEHOG_MOVE2'] = 'bewege Motor %1 und %2 mit Geschwindigkeit %3 und %4 für %5 Sekunden';
+Blockly.Msg['HEDGEHOG_MOVE2_TOOLTIP'] = 'Bewegt zwei Motor in eine bestimmte Richtung';
+
+Blockly.Msg['HEDGEHOG_MOVE2_UNLIMITED'] = 'bewege Motor %1 und %2 mit Geschwindigkeit %3 und %4';
+Blockly.Msg['HEDGEHOG_MOVE2_UNLIMITED_TOOLTIP'] = 'bewegt zwei Motoren solange bis sie explizit gestoppt werden';
+
+Blockly.Msg['HEDGEHOG_FORWARD'] = 'bewege Motor %1 und %2 %3 für %4 Sekunden';
+Blockly.Msg['HEDGEHOG_FORWARD_TOOLTIP'] = 'Bewegt zwei Motoren in eine bestimmte Richtung';
+
+Blockly.Msg['HEDGEHOG_TURN'] = 'wende mit Motor %1 und %2 %3 für %4 Sekunden';
+Blockly.Msg['HEDGEHOG_TURN_TOOLTIP'] = 'wendet den Hedgehog indem zwei Motoren in entgegengesetzter Richtung laufen gelassen werden';
+
+Blockly.Msg['HEDGEHOG_MOTOR_OFF2'] = 'schalte Motoren %1 und %2 aus';
+Blockly.Msg['HEDGEHOG_MOTOR_OFF2_TOOLTIP'] = 'schaltet zwei Motoren gleichzeitig aus';
+
+Blockly.Msg['HEDGEHOG_BRAKE2'] = 'bremse Motoren %1 und %2';
+Blockly.Msg['HEDGEHOG_BRAKE2_TOOLTIP'] = 'bremst zwei Motoren gleichzeitig';
+
+Blockly.Msg['HEDGEHOG_SERVO'] = 'setze Servo %1 auf %2 Grad';
+Blockly.Msg['HEDGEHOG_SERVO_TOOLTIP'] = 'verändert die position eines Servos';
+
+Blockly.Msg['HEDGEHOG_SERVO_OFF'] = 'schalte Servo %1 aus';
+Blockly.Msg['HEDGEHOG_SERVO_OFF_TOOLTIP'] = 'deaktiviert einen Servo';
+
+Blockly.Msg['HEDGEHOG_SPEAKER'] = 'schalte Lautsprecher auf %1 Hz';
+Blockly.Msg['HEDGEHOG_SPEAKER_TOOLTIP'] = 'schaltet den Lautsprecher auf eine bestimmte Frequenz';
+
+Blockly.Msg['HEDGEHOG_SPEAKER_OFF'] = 'schalte Lautsprecher aus';
+Blockly.Msg['HEDGEHOG_SPEAKER_OFF_TOOLTIP'] = 'deaktiviert den Lautsprecher';
+
+Blockly.Msg['HEDGEHOG_PULLUP'] = 'Port %1 pullup %2';
+Blockly.Msg['HEDGEHOG_PULLUP_TOOLTIP'] = 'Setze einen Pullup Widerstand für einen bestimmten IO Port';
+
+Blockly.Msg['HEDGEHOG_READ_ANALOG'] = 'analoger Port %1';
+Blockly.Msg['HEDGEHOG_READ_ANALOG_TOOLTIP'] = 'gibt den Wert eines analogen Ports zurück';
+
+Blockly.Msg['HEDGEHOG_READ_DIGITAL'] = 'digitaler Port %1';
+Blockly.Msg['HEDGEHOG_READ_DIGITAL_TOOLTIP'] = 'gibt den Wert eines analogen Ports zurück';
+
+Blockly.Msg['HEDGEHOG_READ_IMU'] = 'Achse %2 der %1 des Lagesensors';
+Blockly.Msg['HEDGEHOG_READ_IMU_TOOLTIP'] = 'gibt einen oder alle Werte des Lagesensors zurück';
+
+Blockly.Msg['HEDGEHOG_READ_IMU_POSE'] = 'Pose';
+
+Blockly.Msg['HEDGEHOG_READ_IMU_ACCELERATION'] = 'Beschleunigung';
+
+Blockly.Msg['HEDGEHOG_READ_IMU_RATE'] = 'Winkelgeschwindigkeit';
+
+Blockly.Msg['HEDGEHOG_SLEEP'] = 'pausiere für %1 Sekunden';
+Blockly.Msg['HEDGEHOG_SLEEP_TOOLTIP'] = 'pausiert den Programmablauf für eine spezifische Zeit';
+
+Blockly.Msg['HEDGEHOG_CREATE_SCOPE'] = 'iRobot Create scope %1 %2';
+Blockly.Msg['HEDGEHOG_CREATE_SCOPE_TOOLTIP'] = 'Alle Create Blöcke müssen sich innerhalb eines Create oder Create2 scope Blocks befinden';
+
+Blockly.Msg['HEDGEHOG_CREATE2_SCOPE'] = 'iRobot Create2 scope %1 %2';
+
+Blockly.Msg['HEDGEHOG_CREATE_DRIVE_DIRECT'] = 'fahre mit dem Create %1 Sekunden lang mit Geschwindigkeiten %2 & %3';
+Blockly.Msg['HEDGEHOG_CREATE_DRIVE_DIRECT_TOOLTIP'] = 'Fährt den Create für eine bestimmte Zeit';
+
+Blockly.Msg['HEDGEHOG_LEGO_MOTOR_CONFIG'] = 'konfiguriere Motor %1 als Lego-Motor';
+Blockly.Msg['HEDGEHOG_LEGO_MOTOR_CONFIG_TOOLTIP'] = 'konfiguriert einen Motor als Lego-Motor';
+
+Blockly.Msg['HEDGEHOG_LEGO_SENSOR_CONFIG'] = 'konfiguriere Sensor %1 als Lego-Sensor';
+Blockly.Msg['HEDGEHOG_LEGO_SENSOR_CONFIG_TOOLTIP'] = 'konfiguriert einen Sensor als Lego-Sensor';
+
+Blockly.Msg['HEDGEHOG_LEGO_MOVE'] = 'bewege Motor %1 %2 für %3 Sekunden';
+Blockly.Msg['HEDGEHOG_LEGO_MOVE_TOOLTIP'] = 'Bewegt einen Motor in eine bestimmte Richtung';
+
+Blockly.Msg['HEDGEHOG_LEGO_MOVE_UNLIMITED'] = 'bewege Motor %1 %2';
+Blockly.Msg['HEDGEHOG_LEGO_MOVE_UNLIMITED_TOOLTIP'] = 'bewegt einen Motor solange bis er explizit gestoppt wird';
+
+Blockly.Msg['HEDGEHOG_LEGO_MOVE2'] = 'bewege Motor %1 und %2 mit Geschwindigkeit %3 und %4 für %5 Sekunden';
+Blockly.Msg['HEDGEHOG_LEGO_MOVE2_TOOLTIP'] = 'Bewegt zwei Motor in eine bestimmte Richtung';
+
+Blockly.Msg['HEDGEHOG_LEGO_MOVE2_UNLIMITED'] = 'bewege Motor %1 und %2 mit Geschwindigkeit %3 und %4';
+Blockly.Msg['HEDGEHOG_LEGO_MOVE2_UNLIMITED_TOOLTIP'] = 'bewegt zwei Motoren solange bis sie explizit gestoppt werden';
+
+Blockly.Msg['HEDGEHOG_LEGO_FORWARD'] = 'bewege Motor %1 und %2 %3 für %4 Sekunden';
+Blockly.Msg['HEDGEHOG_LEGO_FORWARD_TOOLTIP'] = 'Bewegt zwei Motoren in eine bestimmte Richtung';
+
+Blockly.Msg['HEDGEHOG_LEGO_TURN'] = 'wende mit Motor %1 und %2 %3 für %4 Sekunden';
+Blockly.Msg['HEDGEHOG_LEGO_TURN_TOOLTIP'] = 'wendet den Hedgehog indem zwei Motoren in entgegengesetzter Richtung laufen gelassen werden';
