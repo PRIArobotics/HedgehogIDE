28c6f79dae386988e61ce02382bf551065d87ded
Clemens Koza <koza@pria.at>
2020-01-10 13:16:25 +0100

    fix lints

--- src/components/ide/VisualEditor/blocks/async_procedures_js.js
+++ src/components/ide/VisualEditor/blocks/async_procedures_js.js
@@ -8,38 +8,53 @@ Blockly.Extensions.registerMixin('requires_async_js_function', {
 
 // TODO prevent infinite recursion for recursive functions
 
+function requiresAsyncJsFunction(block: Blockly.Block): boolean {
+  // eslint-disable-next-line no-use-before-define
+  return isAsyncProcedureCall(block) || block.requiresAsyncJsFunction;
+}
+
 function isAsyncProcedureCall(block: Blockly.Block): boolean {
-  if (block.type !== 'procedures_callnoreturn' && block.type !== 'procedures_callreturn')
+  if (
+    block.type !== 'procedures_callnoreturn' &&
+    block.type !== 'procedures_callreturn'
+  )
     return false;
 
-  const defBlock = Blockly.Procedures.getDefinition(block.getProcedureCall(), block.workspace);
+  const defBlock = Blockly.Procedures.getDefinition(
+    block.getProcedureCall(),
+    block.workspace,
+  );
+  // eslint-disable-next-line no-use-before-define
   return isAsyncProcedure(defBlock);
 }
 
 function isAsyncProcedure(block: Blockly.Block): boolean {
-  if (block.type !== 'procedures_defnoreturn' && block.type !== 'procedures_defreturn')
+  if (
+    block.type !== 'procedures_defnoreturn' &&
+    block.type !== 'procedures_defreturn'
+  )
     return false;
 
   // a procedure is async if any descendant is async,
   // i.e. if not every descendant is not async
-  return !block.getDescendants().every(child => !requiresAsyncJsFunction(child));
+  return !block
+    .getDescendants()
+    .every(child => !requiresAsyncJsFunction(child));
 }
 
-function requiresAsyncJsFunction(block: Blockly.Block): boolean {
-  return isAsyncProcedureCall(block) || block.requiresAsyncJsFunction;
-}
-
-const original_procedures_callreturn_generator = Blockly.JavaScript['procedures_callreturn'];
-Blockly.JavaScript['procedures_callreturn'] = block => {
+// eslint-disable-next-line camelcase
+const original_procedures_callreturn_generator =
+  Blockly.JavaScript.procedures_callreturn;
+Blockly.JavaScript.procedures_callreturn = block => {
   let code = original_procedures_callreturn_generator(block)[0];
-  if (isAsyncProcedureCall(block))
-    code = `await ${code}`;
+  if (isAsyncProcedureCall(block)) code = `await ${code}`;
   return [code, Blockly.JavaScript.ORDER_AWAIT];
-}
+};
 
+// eslint-disable-next-line camelcase, no-underscore-dangle
 const original_scrub_ = Blockly.JavaScript.scrub_;
+// eslint-disable-next-line camelcase, no-underscore-dangle
 Blockly.JavaScript.scrub_ = (block, code, opt_thisOnly) => {
-  if (isAsyncProcedure(block))
-    code = `async ${code}`;
-  return original_scrub_(block, code, opt_thisOnly);
-}
+  const newCode = isAsyncProcedure(block) ? `async ${code}` : code;
+  return original_scrub_(block, newCode, opt_thisOnly);
+};
