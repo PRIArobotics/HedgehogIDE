8512db12f49f78be31903820a09db499b6a0b8f1
Clemens Koza <koza@pria.at>
2020-01-23 15:13:34 +0100

    bring async code in line with how the executor APIs are written

--- src/components/misc/AsyncReact.js
+++ src/components/misc/AsyncReact.js
@@ -3,9 +3,9 @@
 
 import * as React from 'react';
 
-export function setState<Props, State>(
+export async function setState<Props, State>(
   component: React.Component<Props, State>,
   partialState: ?$Shape<State> | ((State, Props) => ?$Shape<State>),
 ): Promise<void> {
-  return new Promise(resolve => component.setState(partialState, resolve));
+  await new Promise(resolve => component.setState(partialState, resolve));
 }
