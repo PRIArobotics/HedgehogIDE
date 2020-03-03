e8d57816558bed506dbbbe8f856da1db1bdab761
Clemens Koza <koza@pria.at>
2020-03-03 16:54:46 +0100

    upgrade Workbox to v5

--- src/serviceworker/index.js
+++ src/serviceworker/index.js
@@ -1,16 +1,21 @@
 /* eslint-disable no-restricted-globals */
-/* eslint no-underscore-dangle: ["error", { "allow": ["__precacheManifest"] }] */
+/* eslint no-underscore-dangle: ["error", { "allow": ["__WB_MANIFEST"] }] */
 
-workbox.core.skipWaiting();
-workbox.core.clientsClaim();
+import { skipWaiting, clientsClaim } from 'workbox-core';
+import { precacheAndRoute } from 'workbox-precaching';
+
+const WB_MANIFEST = self.__WB_MANIFEST;
+
+skipWaiting();
+clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(
+const precacheManifest = [].concat(
   [{ url: '/' }, { url: '/executor' }],
-  self.__precacheManifest || [],
+  WB_MANIFEST || [],
 );
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+precacheAndRoute(precacheManifest, {});
