5b2fd8400c1d24525e89367fd1fb8c9310630b39
Clemens Koza <koza@pria.at>
2020-05-06 15:06:52 +0200

    refactor createUser to use async code only, not mixed with callbacks

--- tools/createUser.js
+++ tools/createUser.js
@@ -8,33 +8,35 @@ const input = readline.createInterface({
   output: process.stdout,
 });
 
-export default async function() {
-  const username = await new Promise(res =>
-    input.question('Username: ', res),
-  );
+const question = prompt => new Promise(resolve => {
+  input.question(prompt, resolve);
+});
 
-  let password = await new Promise(res =>
-    input.question('Password: ', res),
-  );
-
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) {
-      console.error(`Error creating user: ${err}`);
-      process.exit(1);
-    }
-
-    // eslint-disable-next-line no-shadow
-    bcrypt.hash(password, salt, async (err, hash) => {
-      if (err) {
-        console.error(`Error creating user: ${err}`);
-        process.exit(1);
-      }
-
-      // Store hash in your password DB.
-      password = hash;
-      const user = await User.create({ username, password });
-      console.log(`Created user with id '${user._id}'`);
-      process.exit(0);
-    });
+// takes a function that takes a callback `(err, result) => ...`
+// as its last parameter and makes it into a function that
+// returns a promise instead
+const promisify = fn => (...args) => new Promise((resolve, reject) => {
+  fn(...args, (err, result) => {
+    if (err) reject(err);
+    else resolve(result);
   });
+});
+
+export default async function() {
+  const username = await question('Username: ');
+  let password = await question('Password: ');
+
+  try {
+    const salt = await promisify(bcrypt.genSalt)(10);
+    const hash = await promisify(bcrypt.hash)(password, salt);
+
+    // Store hash in your password DB.
+    password = hash;
+    const user = await User.create({ username, password });
+    console.log(`Created user with id '${user._id}'`);
+    process.exit(0);
+  } catch (err) {
+    console.error(`Error creating user: ${err}`);
+    process.exit(1);
+  }
 };
