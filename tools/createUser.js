e82b2b1ad11a57db782983ae108347f11934e569
Clemens Koza <koza@pria.at>
2020-05-06 15:22:41 +0200

    proper cleanup uf resources (db & input)

--- tools/createUser.js
+++ tools/createUser.js
@@ -1,16 +1,7 @@
 import readline from 'readline';
 import bcrypt from 'bcryptjs';
 
-import { User } from '../src/server/data/mongodb';
-
-const input = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-const question = prompt => new Promise(resolve => {
-  input.question(prompt, resolve);
-});
+import db, { User } from '../src/server/data/mongodb';
 
 // takes a function that takes a callback `(err, result) => ...`
 // as its last parameter and makes it into a function that
@@ -23,8 +14,18 @@ const promisify = fn => (...args) => new Promise((resolve, reject) => {
 });
 
 export default async function() {
+  const input = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const question = query => new Promise(resolve => {
+    input.question(query, resolve);
+  });
+
   const username = await question('Username: ');
   let password = await question('Password: ');
+  input.close();
 
   try {
     const salt = await promisify(bcrypt.genSalt)(10);
@@ -32,9 +33,12 @@ export default async function() {
 
     // Store hash in your password DB.
     password = hash;
-    const user = await User.create({ username, password });
-    console.log(`Created user with id '${user._id}'`);
-    process.exit(0);
+    try {
+      const user = await User.create({ username, password });
+      console.log(`Created user with id '${user._id}'`);
+    } finally {
+      await db.close();
+    }
   } catch (err) {
     console.error(`Error creating user: ${err}`);
     process.exit(1);
