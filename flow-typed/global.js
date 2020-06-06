1f7fc2fa835abf66fd8f8710f882b48f2fc9f8ed
Clemens Koza <koza@pria.at>
2020-06-06 10:15:36 +0200

    remove stubs in flow-typed/global.js; generate stubs for libs where they weren't auto-generated

--- flow-typed/global.js
+++ flow-typed/global.js
@@ -11,39 +11,3 @@ declare type RefObject<T> = {
 declare type Ref<Instance> =
   | { current: Instance | null }
   | (Instance | null) => mixed;
-
-declare module '@material-ui/core/AppBar' { declare module.exports: any; }
-declare module '@material-ui/core/Avatar' { declare module.exports: any; }
-declare module '@material-ui/core/Badge' { declare module.exports: any; }
-declare module '@material-ui/core/Button' { declare module.exports: any; }
-declare module '@material-ui/core/Container' { declare module.exports: any; }
-declare module '@material-ui/core/CssBaseline' { declare module.exports: any; }
-declare module '@material-ui/core/Dialog' { declare module.exports: any; }
-declare module '@material-ui/core/DialogActions' { declare module.exports: any; }
-declare module '@material-ui/core/DialogContent' { declare module.exports: any; }
-declare module '@material-ui/core/DialogContentText' { declare module.exports: any; }
-declare module '@material-ui/core/DialogTitle' { declare module.exports: any; }
-declare module '@material-ui/core/Divider' { declare module.exports: any; }
-declare module '@material-ui/core/Drawer' { declare module.exports: any; }
-declare module '@material-ui/core/Grid' { declare module.exports: any; }
-declare module '@material-ui/core/Icon' { declare module.exports: any; }
-declare module '@material-ui/core/IconButton' { declare module.exports: any; }
-declare module '@material-ui/core/List' { declare module.exports: any; }
-declare module '@material-ui/core/ListItem' { declare module.exports: any; }
-declare module '@material-ui/core/Menu' { declare module.exports: any; }
-declare module '@material-ui/core/MenuItem' { declare module.exports: any; }
-declare module '@material-ui/core/ListItemAvatar' { declare module.exports: any; }
-declare module '@material-ui/core/ListItemIcon' { declare module.exports: any; }
-declare module '@material-ui/core/ListItemSecondaryAction' { declare module.exports: any; }
-declare module '@material-ui/core/ListItemText' { declare module.exports: any; }
-declare module '@material-ui/core/Paper' { declare module.exports: any; }
-declare module '@material-ui/core/TextField' { declare module.exports: any; }
-declare module '@material-ui/core/Toolbar' { declare module.exports: any; }
-declare module '@material-ui/core/Tooltip' { declare module.exports: any; }
-declare module '@material-ui/core/Typography' { declare module.exports: any; }
-declare module '@material-ui/lab/Alert' { declare module.exports: any; }
-
-declare module 'graphql' { declare module.exports: any; }
-declare module 'graphql/language' { declare module.exports: any; }
-declare module 'material-ui-popup-state' { declare module.exports: any; }
-declare module 'material-ui-popup-state/hooks' { declare module.exports: any; }
