2f7728cbd39c86ac0b4093b55267960936448319
Clemens Koza <koza@pria.at>
2020-06-17 15:18:24 +0200

    improve useLocalStorage - now supports functional updates

--- src/components/misc/hooks/localStorage.js
+++ src/components/misc/hooks/localStorage.js
@@ -2,20 +2,31 @@
 
 import * as React from 'react';
 
-type LocalStorageOptHook<T> = (key: string | null) => [T | void, (T) => void];
-type LocalStorageHook<T> = (key: string) => [T, (T) => void];
+type Setter<T> = ((T => T) | T) => void;
+type LocalStorageOptHook<T> = (key: string | null) => [T | void, Setter<T>];
+type LocalStorageHook<T> = (key: string) => [T, Setter<T>];
+
+// prettier-ignore
+type LocalStorageValue<T> =
+  | {| key: null |}
+  | {| key: string, value: T |};
 
 export function makeLocalStorageOpt<T>(
   deserialize: (string | null) => T,
   serialize: T => string | null,
 ): LocalStorageOptHook<T> {
-  function load(key: string | null): T | void {
-    if (key === null) return undefined;
+  function load(key: string | null): LocalStorageValue<T> {
+    if (key === null) return { key };
 
-    return deserialize(localStorage.getItem(key) ?? null);
+    const value = deserialize(localStorage.getItem(key) ?? null);
+    return { key, value };
   }
 
-  function store(key: string, value: T) {
+  function store(storageValue: LocalStorageValue<T>) {
+    if (storageValue.key === null) return;
+
+    const { key, value } = storageValue;
+
     const serialized = serialize(value);
     if (serialized !== null) {
       localStorage.setItem(key, serialized);
@@ -25,26 +36,37 @@ export function makeLocalStorageOpt<T>(
   }
 
   function useLocalStorage(key: string | null) {
-    const prevKeyRef = React.useRef<string | null>(key);
-    const [state, setStateImpl] = React.useState<T | void>(() => load(key));
+    const [state, setStateImpl] = React.useState<LocalStorageValue<T>>(() => load(key));
 
     // reload state when the key changes
-    if (prevKeyRef.current !== key) {
-      prevKeyRef.current = key;
-      setStateImpl(load(key));
+    React.useEffect(() => {
+      if (state.key !== key) setStateImpl(load(key));
+    }, [key, state.key]);
+
+    // we use a functional update as each update needs to access the key,
+    // which is part of the state
+    function setState(value: (T => T) | T) {
+      // $FlowExpectError
+      const setter: T => T = typeof value === 'function' ? value : () => value;
+
+      setStateImpl(oldValue => {
+        if (oldValue.key === null) return oldValue;
+
+        const newValue = {
+          key: oldValue.key,
+          value: setter(oldValue.value),
+        };
+
+        store(newValue);
+        return newValue;
+      });
     }
 
-    // store state when the value changes
-    function setState(value: T) {
-      if (key === null) return;
-
-      store(key, value);
-      setStateImpl(value);
-    }
+    const value = state.key !== null ? state.value : undefined;
 
     // useCallback: React.useState guarantees stability of setState,
     // mirror that here - at least when the key is stable
-    return [state, React.useCallback(setState, [key, setStateImpl])];
+    return [value, React.useCallback(setState, [setStateImpl])];
   }
 
   return useLocalStorage;
