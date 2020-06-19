68573b2ce38ffd77d53270c039c945b9d6c1bc73
Clemens Koza <koza@pria.at>
2020-06-19 12:48:21 +0200

    slightly improve type of promisify function

--- src/util.js
+++ src/util.js
@@ -1,13 +1,16 @@
 // @flow
+/* eslint-disable import/prefer-default-export */
 
 // takes a function that takes a callback `(err, result) => ...`
-// as its last parameter and makes it into a function that
-// returns a promise instead
-// eslint-disable-next-line import/prefer-default-export
-export const promisify = fn => (...args) =>
-  new Promise((resolve, reject) => {
-    fn(...args, (err, result) => {
-      if (err) reject(err);
-      else resolve(result);
+// as its last parameter and makes it into a function that returns a promise instead
+// Flow doesn't manage to capture the type of `(...args, cb) => void` unfortunately,
+// so the actual type of the promise can not be inferred, and argument types are not checked.
+export function promisify<T>(fn: (...args: any[]) => void): (...args: any[]) => Promise<T> {
+  return (...args) =>
+    new Promise((resolve, reject) => {
+      fn(...args, (err, result) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
     });
-  });
+}
