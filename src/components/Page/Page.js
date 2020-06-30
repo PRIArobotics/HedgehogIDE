dfef36dbd75f05711904a64a5c7177300cfb7b15
Clemens Koza <koza@pria.at>
2020-06-30 11:11:39 +0200

    Revert "Delete unused Page component"
    This reverts commit 62d4c4f6f8ab169c803604e404f2249171c6097e.

---
+++ src/components/Page/Page.js
@@ -0,0 +1,27 @@
+// @flow
+
+import * as React from 'react';
+import withStyles from 'isomorphic-style-loader/withStyles';
+
+import s from './Page.scss';
+
+type PageProps = {|
+  title: string,
+  html: string,
+|};
+
+function Page({ title, html }: PageProps) {
+  return (
+    <div className={s.root}>
+      <div className={s.container}>
+        <h1>{title}</h1>
+        <div
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default withStyles(s)(Page);
