2f3529e88a2ac9ba7216d8ceceb2178ffd1c71c0
Clemens Koza <koza@pria.at>
2020-08-20 10:31:28 +0200

    extract createArray, make the sensors and actuators objects to fit additional data: callbacks for the actuators which are set outside the simulation, and a noise source for sensors

--- src/components/ide/Simulator/simulation/Hedgehog.js
+++ src/components/ide/Simulator/simulation/Hedgehog.js
@@ -1,38 +1,60 @@
 // @flow
 
+type Motor = {
+  power: number,
+  onChange: ((port: number, power: number) => void) | null,
+};
+
+type Servo = {
+  position: number | null,
+  onChange: ((port: number, position: number | null) => void) | null,
+};
+
+type Sensor = {
+  analogValue: number,
+  noiseSource: ((port: number, analogValue: number) => number) | null,
+};
+
+function createArray<T>(length: number, cb: (index: number) => T): T[] {
+  // Array.from({ length: n }, (v, i) => ...):
+  // first parameter is array-like, so `length` is an array length
+  // all values (`v`) are `undefined`, map them to something else.
+
+  return Array.from({ length }, (elem, index) => cb(index));
+}
+
 /**
  * The Hedgehog class represents the controller of a simulated robot.
  * As such, it manages the simulated sensors (whose input values are set by the simulation when changes occur)
  * and actuators (which are set by user programs and queried by the simulation when needed).
  */
 export default class Hedgehog {
-  // Array.from({ length: n }, (v, i) => ...):
-  // first parameter is array-like, so `length` is an array length
-  // all values (`v`) are `undefined`, map them to something else.
-
   // motor (port = 0..4) power, -100..=100
-  motors: number[] = Array.from({ length: 4 }, () => 0);
+  motors: Motor[] = createArray(4, () => ({ power: 0, onChange: null }));
   // servo (port = 0..6) positions, 0..=1000 or null
-  servos: (number | null)[] = Array.from({ length: 6 }, () => null);
+  servos: Servo[] = createArray(6, () => ({ position: null, onChange: null }));
   // sensor (port = 0..16) value inferred from the simulation
   // the value here is "exact"; an analog value read by a user program should
   // have some noise applied to it
-  sensors: number[] = Array.from({ length: 16 }, () => 4095);
+  sensors: Sensor[] = createArray(16, () => ({ analogValue: 4095, noiseSource: null }));
 
   // "public" APIs for the client
 
   moveMotor(port: number, power: number) {
-    this.motors[port] = power;
+    this.motors[port].power = power;
+    this.motors[port].onChange?.(port, power);
   }
 
   setServo(port: number, position: number | null) {
-    this.servos[port] = position;
+    this.servos[port].position = position;
+    this.servos[port].onChange?.(port, position);
   }
 
   getAnalog(port: number): number {
-    // TODO noise
-    const noise = 0;
-    const result = this.sensors[port] + noise;
+    const { analogValue, noiseSource } = this.sensors[port];
+
+    const noise = noiseSource?.(port, analogValue) ?? 0;
+    const result = analogValue + noise;
 
     if (result > 4095) return 4095;
     if (result < 0) return 0;
@@ -46,14 +68,14 @@ export default class Hedgehog {
   // "private" APIs needed by the simulation
 
   getMotor(port: number): number {
-    return this.motors[port];
+    return this.motors[port].power;
   }
 
   getServo(port: number): number | null {
-    return this.servos[port];
+    return this.servos[port].position;
   }
 
   setSensor(port: number, value: number) {
-    this.sensors[port] = value;
+    this.sensors[port].analogValue = value;
   }
 }
