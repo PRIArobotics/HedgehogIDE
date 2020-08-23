a7c601d599f5d7af4e3fd2533435ee7371b0ee53
Clemens Koza <koza@pria.at>
2020-08-23 08:51:23 +0200

    extract DifferentialDrive into drives.js

---
+++ src/components/ide/Simulator/simulation/drives.js
@@ -0,0 +1,48 @@
+// @flow
+
+import Matter from 'matter-js';
+
+import { Point, Hedgehog } from '.';
+
+export class DifferentialDrive {
+  controller: Hedgehog;
+
+  leftWheel: Matter.Body;
+  rightWheel: Matter.Body;
+  body: Matter.Body;
+
+  constructor(
+    controller: Hedgehog,
+    leftWheel: Matter.Body,
+    rightWheel: Matter.Body,
+    body: Matter.Body,
+  ) {
+    this.controller = controller;
+    this.leftWheel = leftWheel;
+    this.rightWheel = rightWheel;
+    this.body = body;
+  }
+
+  applyForce(pos: Point, force: number, cos: number, sin: number) {
+    Matter.Body.applyForce(this.body, pos, {
+      x: force * cos,
+      y: force * sin,
+    });
+  }
+
+  beforeUpdate() {
+    const lPos = this.leftWheel.position;
+    const rPos = this.rightWheel.position;
+    const dx = lPos.x - rPos.x;
+    const dy = lPos.y - rPos.y;
+    const hypot = Math.hypot(dx, dy);
+
+    // cosine and sine of the angle in which the forces are directed
+    // this is normal to the axis of the wheels, therefore [-dy, dx] instead of [dx, dy]
+    const cos = -dy / hypot;
+    const sin = dx / hypot;
+
+    this.applyForce(lPos, this.controller.getMotor(0) / 800, cos, sin);
+    this.applyForce(rPos, this.controller.getMotor(1) / 800, cos, sin);
+  }
+}
