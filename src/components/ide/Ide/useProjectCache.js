15f5a01ee849224008f2101bdce84d3fc57754b4
Clemens Koza <koza@pria.at>
2020-09-17 15:46:35 +0200

    project cache now contains a map of all of the projects assets, loaded into memory as binary

--- src/components/ide/Ide/useProjectCache.js
+++ src/components/ide/Ide/useProjectCache.js
@@ -4,7 +4,12 @@ import * as React from 'react';
 
 import filer, { fs } from 'filer';
 
-import { type FilerRecursiveStatInfo, Project } from '../../../core/store/projects';
+import {
+  type FilerRecursiveStatInfo,
+  type FilerRecursiveDirectoryInfo,
+  Project,
+  getDescendant,
+} from '../../../core/store/projects';
 
 import { useAsyncState } from '../../misc/hooks';
 
@@ -12,6 +17,7 @@ type ProjectCache = {|
   files: FilerRecursiveStatInfo,
   simulatorXml: string | null,
   layoutJson: string | null,
+  assets: Map<string, Uint8Array>,
 |};
 
 export default function useProjectCache(
@@ -23,6 +29,51 @@ export default function useProjectCache(
     if (project === null) {
       setState(null);
       return;
+    }
+
+    async function loadAssets(root: FilerRecursiveStatInfo): Promise<Map<string, Uint8Array>> {
+      const assetsMap: Map<string, Uint8Array> = new Map();
+
+      let assetsFile: FilerRecursiveStatInfo;
+      try {
+        assetsFile = getDescendant(root, '.metadata', 'assets');
+      } catch (err) {
+        console.error(err);
+        return assetsMap;
+      }
+
+      if (!assetsFile.isDirectory()) return assetsMap;
+      // $FlowExpectError
+      const assetsDir: FilerRecursiveDirectoryInfo = assetsFile;
+
+      async function walk(prefix: string, files: FilerRecursiveStatInfo[]) {
+        await Promise.all(
+          files.map(async f => {
+            if (f.isDirectory()) {
+              // $FlowExpectError
+              const dir: FilerRecursiveDirectoryInfo = f;
+              await walk(`${prefix}${f.name}/`, dir.contents);
+            } else {
+              const absolutePath = project.resolve('./.metadata/assets', prefix, f.name);
+              const asset = await fs.promises.readFile(absolutePath);
+              assetsMap.set(`asset:${prefix}${f.name}`, asset);
+            }
+          }),
+        );
+      }
+
+      await walk('', assetsDir.contents);
+
+      return assetsMap;
+    }
+
+    async function loadFilesAndAssets(): Promise<
+      [FilerRecursiveStatInfo, Map<string, Uint8Array>],
+    > {
+      const files = await project.getFiles();
+      const assets = await loadAssets(files);
+
+      return [files, assets];
     }
 
     async function loadSimulatorXml(): Promise<string | null> {
@@ -54,13 +105,13 @@ export default function useProjectCache(
 
     async function loadProjectCache() {
       // load project from the file system
-      const [files, simulatorXml, layoutJson] = await Promise.all([
-        project.getFiles(),
+      const [[files, assets], simulatorXml, layoutJson] = await Promise.all([
+        loadFilesAndAssets(),
         loadSimulatorXml(),
         loadLayoutJson(),
       ]);
 
-      return { files, simulatorXml, layoutJson };
+      return { files, simulatorXml, layoutJson, assets };
     }
 
     setState(loadProjectCache());
