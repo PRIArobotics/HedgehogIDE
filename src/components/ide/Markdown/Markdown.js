35e584e1bf0c3188e1d5476a563d7391f2580895
Clemens Koza <koza@pria.at>
2020-10-15 09:51:13 +0200

    ignore flow error from importing from inside the react-md-editor module

--- src/components/ide/Markdown/Markdown.js
+++ src/components/ide/Markdown/Markdown.js
@@ -7,6 +7,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
 
 import MDEditor, { commands } from '@uiw/react-md-editor';
+// $FlowExpectError
 import md_s from '@uiw/react-md-editor/dist/markdown-editor.css';
 
 import { LanguageMarkdownIcon } from '../../misc/palette';
